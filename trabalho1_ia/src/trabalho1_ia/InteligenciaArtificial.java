594446da44779c6411f5ade2ba4b53a1533a9195
guilherme <guilherme.milan9@gmail.com>
2018-04-29 15:25:11 -0300

    medida de tempo nos algoritmos da IA

--- trabalho1_ia/src/trabalho1_ia/InteligenciaArtificial.java
+++ trabalho1_ia/src/trabalho1_ia/InteligenciaArtificial.java
@@ -7,29 +7,37 @@ public class InteligenciaArtificial {
 
 	//Executa o A*
 	public static ResultadosTeste aEstrela() {
-		Long tempo = 0L;
 		Long numeroPassos = 0L;
 		Tabuleiro tabuleiro = new Tabuleiro();
 		
-		//Aqui fica a lógica do A*
-		// link de um pseudocodigo, parece muito bom, da pra usar de base no desenvolvimento
-		//https://en.wikipedia.org/wiki/A*_search_algorith
+		long tempoInicial = System.nanoTime();
 		
-		ResultadosTeste resultados = new ResultadosTeste(TipoAlgoritmo.A_ESTRELA, tempo, numeroPassos, tabuleiro);
+		//Aqui fica a logica do A*
+		// link de um pseudocodigo, parece muito bom, da pra usar de base no desenvolvimento
+		//https://en.wikipedia.org/wiki/A*_search_algorithm
+		
+		long tempoFinal = System.nanoTime();
+		
+		
+		ResultadosTeste resultados = new ResultadosTeste(TipoAlgoritmo.A_ESTRELA, tempoFinal - tempoInicial, numeroPassos, tabuleiro);
 		return resultados;
 	}
 	
 	//Executa o SMA*
 	public static ResultadosTeste smaEstrela() {
-		Long tempo = 0L;
 		Long numeroPassos = 0L;
 		Tabuleiro tabuleiro = new Tabuleiro();
+		
+		
+		long tempoInicial = System.nanoTime();		
 		
 		//Aqui fica a lógica do SMA*
 		// link de um pseudocodigo parece muito bom, da pra usar de base também
 		// https://en.wikipedia.org/wiki/SMA*
 		
-		ResultadosTeste resultados = new ResultadosTeste(TipoAlgoritmo.SMA_ESTRELA, tempo, numeroPassos, tabuleiro);
+		long tempoFinal = System.nanoTime();
+		
+		ResultadosTeste resultados = new ResultadosTeste(TipoAlgoritmo.SMA_ESTRELA, tempoFinal - tempoInicial, numeroPassos, tabuleiro);
 		return resultados;
 	}
 	
