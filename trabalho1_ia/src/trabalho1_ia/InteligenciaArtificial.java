3c4be0dfde91fa5d71a19f43dcc7da6ebf6f56eb
VictorRivelo <33028110+VictorRivelo@users.noreply.github.com>
2018-04-28 22:28:33 -0300

    Update InteligenciaArtificial.java
    adicionei links para ajudar no desenvolvimento

--- trabalho1_ia/src/trabalho1_ia/InteligenciaArtificial.java
+++ trabalho1_ia/src/trabalho1_ia/InteligenciaArtificial.java
@@ -10,7 +10,9 @@ public class InteligenciaArtificial {
 		Long numeroPassos = 0L;
 		Tabuleiro tabuleiro = new Tabuleiro();
 		
-		//Aqui fica a lÛgica do A*
+		//Aqui fica a l√≥gica do A*
+		// link de um pseudocodigo, parece muito bom, da pra usar de base no desenvolvimento
+		//https://en.wikipedia.org/wiki/A*_search_algorith
 		
 		ResultadosTeste resultados = new ResultadosTeste(TipoAlgoritmo.A_ESTRELA, tempo, numeroPassos, tabuleiro);
 		return resultados;
@@ -22,13 +24,15 @@ public class InteligenciaArtificial {
 		Long numeroPassos = 0L;
 		Tabuleiro tabuleiro = new Tabuleiro();
 		
-		//Aqui fica a lÛgica do SMA*
+		//Aqui fica a l√≥gica do SMA*
+		// link de um pseudocodigo parece muito bom, da pra usar de base tamb√©m
+		// https://en.wikipedia.org/wiki/SMA*
 		
 		ResultadosTeste resultados = new ResultadosTeste(TipoAlgoritmo.SMA_ESTRELA, tempo, numeroPassos, tabuleiro);
 		return resultados;
 	}
 	
-	//aplica a heurÌstica pra selecionar o melhor movimento
+	//aplica a heur√≠stica pra selecionar o melhor movimento
 	private static Tabuleiro getMelhorMovimento(List<Tabuleiro> movimentosValidos) {
 		return movimentosValidos.get(0);
 	}
