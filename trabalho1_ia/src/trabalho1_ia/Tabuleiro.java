58124193881b6f2179f7f0ff715452f0ffe3c4da
VictorRivelo <33028110+VictorRivelo@users.noreply.github.com>
2018-04-28 22:57:53 -0300

    Update Tabuleiro.java
    duvida no moviementoValido.add

--- trabalho1_ia/src/trabalho1_ia/Tabuleiro.java
+++ trabalho1_ia/src/trabalho1_ia/Tabuleiro.java
@@ -30,10 +30,77 @@ public class Tabuleiro {
 	
 	public ArrayList<Tabuleiro> getMovimentosValidos(int linha, int coluna) {
 		ArrayList<Tabuleiro> movimentosValidos = new ArrayList<Tabuleiro>();
+		int projetoMovimentoLinha, projetoMovimentoColuna;
 		
 		//Aqui fica o calculo de todos os movimentos válidos do cavalo
 		
+		//movimentos para cima
+		//anda pra cima - direita
+		projetoMovimentoLinha = linha+2;
+		projetoMovimentoColuna = coluna+1;
+		if (Valido(projetoMovimentoLinha,projetoMovimentoColuna))
+		//movimentosValidos.add(??)
+		
+		//anda pra cima - esquerda
+		projetoMovimentoLinha = linha+2;
+		projetoMovimentoColuna = coluna-1;
+		if (Valido(projetoMovimentoLinha,projetoMovimentoColuna))
+		//movimentosValidos.add(??)
+		
+		//anda pra cima - direita
+		projetoMovimentoLinha = linha+2;
+		projetoMovimentoColuna = coluna+1;
+		if (Valido(projetoMovimentoLinha,projetoMovimentoColuna))
+		//movimentosValidos.add(??)
+
+		//movimentos para direita
+		//anda pra direita - cima 
+		projetoMovimentoLinha = linha+1;
+		projetoMovimentoColuna = coluna+2;
+		if (Valido(projetoMovimentoLinha,projetoMovimentoColuna))
+		//movimentosValidos.add(??) 	
+			
+		//anda pra diretia - baixo
+		projetoMovimentoLinha = linha-1;
+		projetoMovimentoColuna = coluna+2;
+		if (Valido(projetoMovimentoLinha,projetoMovimentoColuna))
+		//movimentosValidos.add(??) 	
+		
+		//movimentos para esquerda
+		//anda pra esquerda - cima 
+		projetoMovimentoLinha = linha+1;
+		projetoMovimentoColuna = coluna-2;
+		if (Valido(projetoMovimentoLinha,projetoMovimentoColuna))
+		//movimentosValidos.add(??) 	
+			
+		//anda pra esquerda - baixo
+		projetoMovimentoLinha = linha-1;
+		projetoMovimentoColuna = coluna-2;
+		if (Valido(projetoMovimentoLinha,projetoMovimentoColuna))
+		//movimentosValidos.add(??) 
+			
+		//movimentos para baixo
+		//anda pra baixo - esquerda
+		projetoMovimentoLinha = linha-2;
+		projetoMovimentoColuna = coluna-1;
+		if (Valido(projetoMovimentoLinha,projetoMovimentoColuna))
+		//movimentosValidos.add(??) 
+
+		//anda pra baixo - direita
+		projetoMovimentoLinha = linha-2;
+		projetoMovimentoColuna = coluna+1;
+		if (Valido(projetoMovimentoLinha,projetoMovimentoColuna))
+		//movimentosValidos.add(??) 
+		
 		return movimentosValidos;
+	}
+	
+	public boolean Valido (int linha, int coluna) {
+		if(linha<nLinhas && coluna<nColunas && linha>0 && coluna>0 && tabuleiro[linha][coluna] == 0) {
+		//o movimento é valido
+		return true;
+		}
+	return false;
 	}
 	
 }
